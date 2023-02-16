271dfc75d393f432d8a86b0f4c01644fc6321750
ignaciozhetta <zagu4988@gmail.com>
2023-02-16 20:21:38 -0300

    env modification

--- db/connection.ts
+++ db/connection.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
 
-const db = new Sequelize('dbnode', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.HOST,
     dialect: 'mariadb',
     // logging: false,
 });
--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('dbnode', 'root', '', {
-    host: 'localhost',
+const db = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.HOST,
     dialect: 'mariadb',
     // logging: false,
 });
